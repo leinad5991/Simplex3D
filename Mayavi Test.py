95e9df30ac64a9f2eb329ec3eab98e3d350b4a5b
leinad5991 <leinad5991@hotmail.com>
2015-11-12 00:55:24 +0100

    Playing around

--- Mayavi Test.py
+++ Mayavi Test.py
@@ -1,16 +1,18 @@
-# Author: Prabhu Ramachandran <prabhu [at] aero.iitb.ac.in>
-# Copyright (c) 2009, Enthought, Inc.
-# License: BSD Style.
 
 from mayavi import mlab
 from tvtk.tools import visual
 import numpy as np
 # Create a figure
 f = mlab.figure(size=(500,500))
+
 # Tell visual to use this as the viewer.
 visual.set_viewer(f)
 
-
+a=np.arange(-4,4,0.1)
+x = a
+y = np.sin(a)
+z = a*0
+s = mlab.plot3d(x,y,z)
 # Even sillier animation.
 b1 = visual.sphere()
 b2 = visual.box(x=4., color=visual.color.red)
@@ -22,7 +24,9 @@ b1.v = 5.0
 def anim():
     i=0
     while True:
+        s.mlab_source.set(y=np.sin(a+i/10.))
         i+=1
+        f.scene.camera.azimuth(0.5)
         b1.x=2*np.sin(i/10.)
         yield
 
