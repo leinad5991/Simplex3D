8df060068dfce3fea3495d27a5f571fc51d8b326
leinad5991 <leinad5991@hotmail.com>
2015-11-14 17:16:26 +0100

    demo for scalar plot

--- Mayavi Test.py
+++ Mayavi Test.py
@@ -3,25 +3,26 @@ from mayavi import mlab
 from tvtk.tools import visual
 import numpy as np
 # Create a figure
-f = mlab.figure(size=(500,500))
+
+f = mlab.figure(size=(1000,1000))
 
 # Tell visual to use this as the viewer.
-visual.set_viewer(f)
-
-a=np.arange(-4,4,0.1)
-x = a
-y = np.sin(a)
-z = a*0
-s = mlab.plot3d(x,y,z)
-# Even sillier animation.
-b1 = visual.sphere()
-b2 = visual.box(x=4., color=visual.color.red)
-b3 = visual.box(x=-4, color=visual.color.red)
-b1.v = 5.0
-
 @mlab.show
 @mlab.animate(delay=20)
 def anim():
+    visual.set_viewer(f)
+
+    a=np.arange(-4,4,0.1)
+    x = a
+    y = np.sin(a)
+    z = a*0
+    s = mlab.plot3d(x,y,z)
+    # Even sillier animation.
+    b1 = visual.sphere()
+    b2 = visual.box(x=4., color=visual.color.red)
+    b3 = visual.box(x=-4, color=visual.color.red)
+
+
     i=0
     while True:
         s.mlab_source.set(y=np.sin(a+i/10.))
@@ -30,5 +31,11 @@ def anim():
         b1.x=2*np.sin(i/10.)
         yield
 
-# Run the animation.
-anim()
+def scalar():
+    x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+    s = 2*x**2+y**2+z**2
+
+    mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+
+scalar()
+#anim()
