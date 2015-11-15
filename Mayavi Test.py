d96819bd2ce11c94d85559b8c8901ddaa102e868
leinad5991 <leinad5991@hotmail.com>
2015-11-15 15:03:21 +0100

    Es funktioniert einigermassen, wenn man aber noch die linien zwichen den Punkten zeichnen will, wartet Mayavi nicht ab und die ganze animation kommt zum stillstand :(

--- Mayavi Test.py
+++ Mayavi Test.py
@@ -9,6 +9,46 @@ f = mlab.figure(size=(1000,1000))
 # Tell visual to use this as the viewer.
 @mlab.show
 @mlab.animate(delay=20)
+
+
+
+
+
+def initialize():
+    coord=[[0,0,0],[1,0,0],[0,0,1],[0,1,0]]
+    #Colors
+    red=(1,0,0)
+
+    print(1)
+    visual.set_viewer(f)
+    b=[]
+    l=[]
+    yield
+    for i in coord:
+        b.append(visual.sphere(pos=i,radius=0.05))
+        yield
+    '''
+    for i in coord:
+        for j in coord:
+            if i!=j:
+                l.append(mlab.plot3d( [i[0],(i[0]+j[0])/2.,j[0]] ,[i[1],(i[1]+j[1])/2.,j[1]] ,[i[2],(i[2]+j[2])/2.,j[2]] ,tube_radius=0.01,color=red))
+                yield
+
+'''
+    print("a")
+    t=np.arange(1,1000,0.1)
+    nn=[]
+    for j in t: nn.append([[0.,np.sin(j/10.),np.sin(j/10.)],[np.sin(j/10.),0.,0.],[0.,0.,np.sin(j/10.)],[0.,np.sin(j/10.),0.]])
+
+    
+    for i in nn:
+        for j in range(4):
+            print(i[0][1])
+            b[j].x=i[j][0]
+            b[j].y=i[j][1]
+            b[j].z=i[j][2]
+            yield
+
 def demoanim():
     visual.set_viewer(f)
 
@@ -30,25 +70,18 @@ def demoanim():
         f.scene.camera.azimuth(0.5)
         b1.x=2*np.sin(i/10.)
         yield
-def anim():
-    #Colors
-    red=(1,0,0)
 
-
+def anim(coords,obj):
     visual.set_viewer(f)
-    coord=[[0,0,0],[1,0,0],[0,0,1],[0,1,0]]
-    b=[]
-    l=[]
-    for i in coord: b.append(visual.sphere(pos=i,radius=0.1))
+    print(2)
+    b=obj[0]
+    l=obj[1]
+    print(1)
+    for c in coords:
+        for i in b:
 
-    for i in coord:
-        for j in coord:
-            if i!=j:
-                l.append(mlab.plot3d( [i[0],(i[0]+j[0])/2.,j[0]] ,[i[1],(i[1]+j[1])/2.,j[1]] ,[i[2],(i[2]+j[2])/2.,j[2]] ,tube_radius=0.01,color=red))
-
-    mlab.show()
-
-
+            i.mlab_source.set(x=c[0],y=c[1],z=c[2])
+            yield
 
 def scalar():
     x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
@@ -59,4 +92,6 @@ def scalar():
     mlab.show()
 
 #scalar()
-anim()
+print(100)
+initialize()
+#demoanim()
