71ca412f29f1eb584f58b5a18b467bbf62b4fc4f
Philipp <philipp.warzecha@gmail.com>
2015-11-17 19:35:50 +0100

    Solver gibt Array für MayaVi aus, sonst nur kleinkram

--- SimplexDownhill.py
+++ SimplexDownhill.py
@@ -4,7 +4,7 @@ import numpy as np
 __author__ = 'Philipp Warzecha'
 
 
-# Donwhill Simplex in 3D
+# Downhill Simplex in 3D
 
 
 """
@@ -13,15 +13,20 @@ ToDo:
 2. **!DONE! wrap everything nicely together
 3. more test functions
 4. **!DONE! random starting points?
-5. make compatible with maya vi (!!important!!)
+5. **!DONE! make compatible with maya vi (!!important!!)
 6. port to 2.7 without future
 
 Changelog:
 
+    Version 0.4:
+        - rewrote stop criteria
+        - solver method returns points for MayaVi(as a python array)
+        - different starting point generation
+
     Version 0.3:
         - added random starting points, not really sure if thats better
         - restructured solver method a bit, still needs work
-        - added two test functions. Styblinski Tang is doable, only one global minimum.
+        - added two test functions. Styblinski Tang is doable, but has local minima.
           Schwefel is harder and has many local minima
         - corrected small errors
 
@@ -50,13 +55,13 @@ def rosenbrock(x):
     return abs(100 * (x[1] - x[0] ** 2) ** 2 + (x[0] - 1) ** 2 + 100 * (x[2] - x[1] ** 2) ** 2 + (x[1] - 1) ** 2)
 
 
-# Minimum: sytblinski_tang(-2.903534,-2.903534,-2.903534) = -117,49797, only global minimum
+# Minimum: sytblinski_tang(-2.903534,-2.903534,-2.903534) = -117,49797, global minimum, some local minima
 # points remain within a distance higher than 10^-9
 def styb_tang(x):
     return sum(x ** 4 - 16.0 * x ** 2 + 5.0 * x) * 0.5
 
 
-# Minimum: schwefel(420.9687,420.9687,420.9687= = 0, many local minima
+# Minimum: schwefel(420.9687,420.9687,420.9687) = 0, many local minima
 def schwefel(x):
     return 418.9829 * 3 - sum(x * np.sin(np.sqrt(abs(x))))
 
@@ -71,6 +76,7 @@ def simplex(x, testfn):
     f_x0 = testfn(x[0])
     f_xreflect = testfn(x_reflect[0])
     f_xworst = testfn(x[last])
+
     # reflected value is better than worst but not better than best value
     if f_xworst >= f_xreflect > f_x0:
         x[last] = x_reflect[0]
@@ -89,29 +95,34 @@ def simplex(x, testfn):
         x[:] = (x[0] + x[:]) * 0.5
 
 
-"""solver uses the simplex downhill algorithm. func is the function to search through for the optimum.
+"""
+   solver uses the simplex downhill algorithm. func is the function to search through for the optimum.
    points is a list of starting points(NxM numpy array).
-   stop_value ist the distance between the worst and the best point.
+   stop_value ist the distance between the function value of the worst and the best point.
    failure is the number of maximum iterations before the algorithm stops
+   maya_points is an array of all points through the search
 """
 def solver(func, points, stop_value, failure):
+    maya_points = []
     i = 0
-    while abs(points[-1, 0] - points[0, 0]) > stop_value or abs(points[-1, 1] - points[0, 1]) > stop_value \
-            or abs(points[-1, 1] - points[0, 1]) > stop_value:
+    while abs(func(points[-1]) - func(points[0])) > stop_value:
+        maya_points.append(points)
         points = np.asarray(sorted(points, key=func))
         simplex(points, func)
         i += 1
-        if i >= failure: break
-    if i < failure:
-        print "Miminum gefunden bei: \n", points
-        print "\nFunktionswerte: ", np.array([func(points[0]), func(points[1]), func(points[2]), func(points[3])])
-        print "\nAnzhal Schritte: ", i
-    else:
-        print "Abbruch nach", i, "Schritten:"
-        print "Punkte: \n", points
-        print "Funktionswerte: ", np.array([func(points[0]), func(points[1]), func(points[2]), func(points[3])])
+        if i >= failure:
+            print "Abbruch nach", i, "Schritten:"
+            print "Punkte: \n", points
+            print "Funktionswerte: ", np.array([func(points[0]), func(points[1]), func(points[2]), func(points[3])])
+            return maya_points
+    print "Miminum gefunden bei: \n", points
+    print "\nFunktionswerte: ", np.array([func(points[0]), func(points[1]), func(points[2]), func(points[3])])
+    print "\nAnzhal Schritte: ", i
+    return maya_points
+
 
 # start points
-start = np.random.rand(4, 3)
+n = 6
+start = n * np.random.rand(4, 3) - n/2
 # start = np.array([[-15.0, 20.0, -15.0], [-5.0, -8.0, -2.0], [5.0, -20.0, 6.0], [10.0, -5.0, 3.0]])
-solver(styb_tang, start, 10 ** (-8), 10000)
+solver(styb_tang, start, 10 ** (-10), 10000)
